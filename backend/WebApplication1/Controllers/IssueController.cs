464dccc0c9fc9cc5e6dd3839db95d10d53afd9de
PontusSandh <pontus.sandh@consid.se>
2026-02-03 13:42:33 +0100

    empty controller

---
+++ backend/WebApplication1/Controllers/IssueController.cs
@@ -0,0 +1,10 @@
+﻿using Business.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace EmberWebApi.Controllers
+{
+    public class IssueController
+    {
+    }
+}
