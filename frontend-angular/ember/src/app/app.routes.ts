5e240218a9b4c20c4c04a7ec81b72afed8294c2d
sannaconsid <sanna.lundqvist@consid.se>
2026-02-13 11:53:28 +0100

    styles and navigation

--- frontend-angular/ember/src/app/app.routes.ts
+++ frontend-angular/ember/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('../content/home/home').then(m => m.HomeComponent) },
+    { path: 'management', loadComponent: () => import('../content/management/management').then(m => m.ManagementComponent) },
+];
