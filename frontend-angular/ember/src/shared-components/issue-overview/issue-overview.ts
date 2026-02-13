982097dd0f1a14b4d790f605acc99d6d50bdbbcf
sannaconsid <sanna.lundqvist@consid.se>
2026-02-13 15:14:21 +0100

    pipe

--- frontend-angular/ember/src/shared-components/issue-overview/issue-overview.ts
+++ frontend-angular/ember/src/shared-components/issue-overview/issue-overview.ts
@@ -1,10 +1,11 @@
 import { Component, input, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { IssueOverviewData } from './issue-overview.types';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-issue-overview',
-  imports: [MatCardModule],
+  imports: [MatCardModule, DatePipe],
   templateUrl: './issue-overview.html',
   styleUrl: './issue-overview.scss',
 
